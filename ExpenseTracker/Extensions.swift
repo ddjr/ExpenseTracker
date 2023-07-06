2db9ac6c2d93ca0722e2c389d04370189d570e15
David Daly <ddalyjr@gmail.com>
2023-07-06 17:26:28 -0400

    first commit - add color extension

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by David Daly on 7/6/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
