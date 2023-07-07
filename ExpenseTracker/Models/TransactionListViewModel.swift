3a344045850c574fc3c81330d8a8706ff212ab27
David Daly <ddalyjr@gmail.com>
2023-07-07 12:39:43 -0400

    Add transaction chart view

--- ExpenseTracker/Models/TransactionListViewModel.swift
+++ ExpenseTracker/Models/TransactionListViewModel.swift
@@ -10,6 +10,7 @@ import Combine
 import Collections
 
 typealias TransactionGroup = OrderedDictionary<String, [Transaction]>
+typealias TransactionPrefixSum = [(String, Double)]
 
 final class TransactionListViewModel: ObservableObject {
     @Published var transactions: [Transaction] = []
@@ -46,9 +47,32 @@ final class TransactionListViewModel: ObservableObject {
             }
             .store(in: &cancellables)
     }
+    
     func groupTransactionsByMonth() -> TransactionGroup {
         guard !transactions.isEmpty else { return [:] }
         let groupedTransactions = TransactionGroup(grouping: transactions, by: \.month)
         return groupedTransactions
+    }
+    
+    func accumulateTransactions() -> [Double] {
+        guard !transactions.isEmpty else { return [] }
+        print("accumulateTransactions")
+        
+        let today = "02/17/2022".dateParsed() // Date()
+        let dateInterval = Calendar.current.dateInterval(of: .month, for: today)!
+        print("dateInterval", dateInterval)
+        
+        var sum: Double = .zero
+        var cumulativeSum: [Double] = []
+        let secondsInADay: Double = 60*60*24
+        for date in stride(from: dateInterval.start, to: today, by: secondsInADay) {
+            let dailyExpenses = transactions.filter({ $0.dateParsed == date && $0.isExpense })
+            let dailyTotal = dailyExpenses.reduce(0) { $0 - $1.signedAmount }
+            sum += dailyTotal
+            cumulativeSum.append(sum)
+            print(date.formatted(), "Daily Total: ", dailyTotal, " Sum: ", sum)
+        }
+
+        return cumulativeSum
     }
 }
--- ExpenseTracker/Views/ContentView.swift
+++ ExpenseTracker/Views/ContentView.swift
@@ -6,16 +6,24 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    var demoData: [Double] = [80, 50, 40, 60, 120, 90, 200, 80, 50, 40, 60, 120, 90, 200]
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading, spacing: 24) {
-                    // MARK: Title
+                    // MARK: Tit
                     Text("Overview")
                         .font(.title2)
                         .bold()
+                    
+                    // MARK: Chart
+                    let data = transactionListVM.accumulateTransactions()
+                    let total = data.last?.rounded() ?? 0
+                    LineChartView(data: data, title: total.formatted(.currency(code: "USD")), form: ChartForm.extraLarge)
                     
                     // MARK: Transaction List
                     RecentTransactionListView()
